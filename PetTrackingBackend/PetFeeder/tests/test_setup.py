7b9415da512db4b29008671e70e392062a650cdf
Tran, Vinh An <83594817+anvinh01@users.noreply.github.com>
2023-08-01 23:12:23 +0200

    starting test

---
+++ PetTrackingBackend/PetFeeder/tests/test_setup.py
@@ -0,0 +1,5 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+
+
--- PetTrackingBackend/PetTrackingBackend/settings.py
+++ PetTrackingBackend/PetTrackingBackend/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'PetTrackingBackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['PetFeeder'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
