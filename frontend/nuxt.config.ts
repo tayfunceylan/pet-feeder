202c6b7846b25027d1436726b8c0c868c5ec6b56
Tayfun <tayfun.ceylan@outlook.de>
2023-08-28 00:00:20 +0200

    ssr false

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   css: [
     'vuetify/lib/styles/main.sass',
